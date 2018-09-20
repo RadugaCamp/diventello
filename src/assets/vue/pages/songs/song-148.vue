912412e81fa8b00a8c3f3a64757e654e03e07d3c
Vladimir Pavlychev <vladimir.pavlychev@osarts.ru>
2018-09-20 17:02:57 +0300

    :dart: Add: 141-152 :notes: songs #89

---
+++ src/assets/vue/pages/songs/song-148.vue
@@ -0,0 +1,23 @@
+<template>
+  <f7-page>
+    <f7-navbar title="148. Хвала Тебе" back-link="Back" sliding></f7-navbar>
+    <f7-block>
+      <p>
+        <br>
+          //: Хвала Тебе, наш Господь!<br />
+          Хвала Тебе, Бог Святой!<br />
+          Хвала за Иисуса, Сына Твоего! ://<br />
+        <br>
+          //: Теперь слабый скажет: я силён,<br />
+          Бедный скажет: я богат,<br />
+          Благодаря тому, что Бог нам дал! :// <br />
+        <br>
+          Хвала!<br />
+        <br>          
+      </p>
+    </f7-block>
+  </f7-page>
+</template>
+<script>
+export default {};
+</script>
